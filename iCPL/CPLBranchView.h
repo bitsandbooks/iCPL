f94ca8ffcc499e56836c87f30744cfe18804fadd
Rob Dumas <rob@bitsandbooks.com>
2012-02-14 22:08:54 -0600

    Added simple schedule and a UIScrollView to CPLBranchView.
    BranchList.plist:
    - Change the values for all scheduleType fields to number strings (0 through 5), because it allows us to use a simple switch statement in CPLBranchView.
    
    CPLBranchView:
    - Added a UIScrollView. Added two UITextViews, one of which is static (days of the week), while the other will be filled with the schedule, from fillScheduleTextArea:.
    - Added a new method, fillScheduleTextArea, which checks the branch's scheduleType and puts the appropriate text into scheduleTextView.
    
    Signed-off-by: Rob Dumas <rob@bitsandbooks.com>

--- iCPL/CPLBranchView.h
+++ iCPL/CPLBranchView.h
@@ -10,20 +10,24 @@
 
 @interface CPLBranchView : UIViewController <UIActionSheetDelegate> {
 	NSDictionary *branchDetails;
+	IBOutlet UIScrollView *scrollView;
 	IBOutlet UILabel *fullNameLabel;
 	IBOutlet UITextView *streetAddressTextView;
 	IBOutlet UILabel *phoneStringLabel;
 	IBOutlet UIImageView *imageView;
+	IBOutlet UITextView *scheduleTextView;
 	IBOutlet UIBarButtonItem *actionButton;
 }
 
 - (IBAction)actionButtonTapped:(id)sender;
 
 @property (retain, nonatomic) NSDictionary *branchDetails;
+@property (retain, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (retain, nonatomic) IBOutlet UILabel *fullNameLabel;
 @property (retain, nonatomic) IBOutlet UITextView *streetAddressTextView;
 @property (retain, nonatomic) IBOutlet UILabel *phoneStringLabel;
 @property (retain, nonatomic) IBOutlet UIImageView *imageView;
+@property (retain, nonatomic) IBOutlet UITextView *scheduleTextView;
 @property (retain, nonatomic) IBOutlet UIBarButtonItem *actionButton;
 
 @end
